ebceb951831c2210a4f0579c4a9e1a3126ea4ffa
Mahima <mahima@gmail.com>
2016-02-01 12:28:22 +0530

    added text on images

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger').addEventListener('click',drawer, false);
+         function  drawer(){
+        	document.getElementById('drawer').classList.toggle('drawer_left');
+        }
