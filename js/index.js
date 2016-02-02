bc7e87c2192724166634d93229bf77ef829825ea
Mahima <mahima@gmail.com>
2016-02-02 18:10:05 +0530

    footer designs

--- js/index.js
+++ js/index.js
@@ -2,3 +2,8 @@ document.getElementById('hamburger').addEventListener('click',drawer, false);
          function  drawer(){
         	document.getElementById('drawer').classList.toggle('drawer_left');
         }
+
+       document.getElementById('header_right').addEventListener('click',drawer2, false);
+            function  drawer2(){
+            document.getElementById('drawer2').classList.toggle('drawer_right');
+            }
