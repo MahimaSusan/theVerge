e2114d5fabb438dbda1e6e550941a9c21f8f18bf
Mahima <mahima@gmail.com>
2016-02-03 18:28:04 +0530

    responsive first stage

--- js/index.js
+++ js/index.js
@@ -7,3 +7,8 @@ document.getElementById('hamburger').addEventListener('click',drawer, false);
             function  drawer2(){
             document.getElementById('drawer2').classList.toggle('drawer_right');
             }
+
+             document.getElementById('down_arrow_a').addEventListener('click',drop_arrow_nav, false);
+            function  drop_arrow_nav(){
+            document.getElementById('drop_arrow_nav').classList.toggle('drawer_down');
+            }
